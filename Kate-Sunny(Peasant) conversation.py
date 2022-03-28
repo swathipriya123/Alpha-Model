1640cafc70e3ae73328d5e8698511137abf6558a
Prachi Manwar <94181169+prachi24s@users.noreply.github.com>
2022-03-28 01:30:19 -0500

    Kate-Sunny(Peasant) Conversation
    This file contains the conversation between Player(Kate) and Peasant (Sunny).

---
+++ Kate-Sunny(Peasant) conversation.py
@@ -0,0 +1,14 @@
+
+
+action('WalkTo(Kate, Sunny)')
+action('SetDialog(Kate: Hello! Can you tell me where is the near by medicine shop?)')
+action('ShowDialog()')
+action('Wait(4.5)')
+action('HideDialog()')
+action('ShowDialog()')
+action('SetRight(Kate)')
+action('SetLeft(Sunny)')
+action('SetDialog(Sunny : Hi! Sure, just go to brown house door, there you can get medicine. )')
+action('SetDialog(Kate: What Thank you so much! Have a good day. )')
+action('SetDialog(Sunny: Welcome, same2you)')
+action('Wait(5)')
