67f158bbaef7528aab070f92aac8da898f70f825
swathipriya123 <99422042+swathipriya123@users.noreply.github.com>
2022-03-27 23:55:57 -0500

    Character peasant

---
+++ character peasant.py
@@ -0,0 +1,9 @@
+#Character peasant 
+
+action('CreateCharacter(Peasant)')
+action('SetClothing(Peasant)',False)
+action('SetExpression(Peasant,Sad)',False)
+action('SetHairStyle(Peasant,Long)')
+action('SetSkinColor(Peasant,0)')
+action('SetEyeColor(Peasant,Red)')
+action('SetRight(Peasant)')
