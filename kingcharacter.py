6b29aefe0a6f8b935f00a010e93fd46bad0e3764
swathipriya123 <99422042+swathipriya123@users.noreply.github.com>
2022-04-05 10:08:14 -0500

    king character.py
    added king character and his clothing and out fit, hair style, Expression, SkinColor, Eyecolor, place for character

---
+++ kingcharacter.py
@@ -0,0 +1,11 @@
+#king character 
+
+
+action('CreatePlace(CastleBedroom,CastleBedroom)')
+action('CreateCharacter(King)')
+action('SetClothing(King,B)',False)
+action('SetExpression(King,Happy)',False)
+action('SetHairStyle(King,Spiky)')
+action('SetSkinColor(King,0)')
+action('SetEyeColor(King,blue)')
+action('SetRight(King)')
